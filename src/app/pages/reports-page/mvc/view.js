aceddd41c75489814e8b26c8eebe74ee6e171330
Serhii Nosko1 <serhii_nosko1@epam.com>
2018-01-05 14:45:33 +0200

    some changes

--- src/app/pages/reports-page/mvc/view.js
+++ src/app/pages/reports-page/mvc/view.js
@@ -3,8 +3,12 @@ import Template from '../template/template.hbs';
 import Highcharts from 'highcharts';
 
 export class View {
-  constructor(data){
+  constructor(data) {
     this.controller = new Controller(data);
+  }
+
+  drawChart(data){
+
   }
 
   render() {
@@ -19,10 +23,12 @@ export class View {
       chart: {
         type: 'column',
         backgroundColor: '#2A3F50',
-        width: 570
+        width: 570,
+        height: 350
       },
       xAxis: {
         type: 'category',
+        categories: ['URGENT', 'HIGH', 'MIDDLE', 'LOW', 'FAILED'],
         lineColor: '#fff',
         tickWidth: 0,
         labels: {
@@ -31,7 +37,7 @@ export class View {
             fontFamily: 'PT Sans, sans-serif',
             fontSize: '13px'
           }
-        }
+        },
       },
       yAxis: {
         title: '',
@@ -47,11 +53,28 @@ export class View {
         }
       },
       legend: {
-        enabled: false
+        symbolRadius: 0,
+        symbolHeight: 8,
+        symbolWidth: 8,
+        margin: 50,
+        style: {
+          display: 'flex',
+          alignItems: 'center'
+        },
+        itemStyle: {
+          color: '#8da5b8',
+          fontWeight: 'normal',
+          fontSize: 12
+        },
+        itemHoverStyle: {
+          color: '#82c7e0'
+        }
       },
       plotOptions: {
         series: {
           borderWidth: 0,
+          pointWidth: 40,
+          groupPadding: 0.5,
           dataLabels: {
             enabled: false,
             format: '{point.y}'
@@ -62,33 +85,61 @@ export class View {
       tooltip: {
         backgroundColor: '#dbeaf5',
         borderColor: '#dbeaf5',
-        headerFormat: '<span class="report__tooltip-title">{point.name}</span><br>',
-        pointFormat: '<span class="report__tooltip-span">Tasks</span>: <b>{point.y}</b><br/>'
+        formatter: function () {
+          return `<span class="report__tooltip-title">${this.series.name}</span><br><span class="report__tooltip-span">Tasks</span>: ${this.series.data[0].y}<br/>`
+        }
       },
 
       series: [{
-        name: 'Brands',
+        name: 'URGENT',
         data: [{
           name: 'URGENT',
-          y: data[0],
-          color: '#F75C4C'
-        }, {
-          name: 'HIGH',
-          y: data[1],
-          color: '#FFA841'
-        }, {
-          name: 'MIDDLE',
-          y: data[2],
-          color: '#FDDC43'
-        }, {
-          name: 'LOW',
-          y: data[3],
-          color: '#1ABC9C'
-        }, {
-          name: 'FAILED',
-          y: data[4],
-          color: '#8da5b8'
+          y: 1,
+          x: 0
+        },
+          {
+            name: 'URGENT',
+            y: 4,
+            x: 0
+          },
+          {
+            name: 'URGENT',
+            y: 6,
+            x: 0
+          }],
+        color: '#F75C4C',
+        zones: [{
+          value: 4,
+          color: 'blue'
         }]
+      }, {
+        name: 'HIGH',
+        data: [{
+          name: 'HIGH',
+          y: data[1]
+        }],
+        color: '#FFA841'
+      }, {
+        name: 'MIDDLE',
+        data: [{
+          name: 'MIDDLE',
+          y: data[2]
+        }],
+        color: '#FDDC43'
+      }, {
+        name: 'LOW',
+        data: [{
+          name: 'LOW',
+          y: data[3]
+        }],
+        color: '#1ABC9C'
+      }, {
+        name: 'FAILED',
+        data: [{
+          name: 'FAILED',
+          y: data[4]
+        }],
+        color: '#8da5b8'
       }]
     });
   }
