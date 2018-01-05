bd09a9dcf0bb4ebd2b5d840e63eba30fb80d8a21
Serhii Nosko1 <serhii_nosko1@epam.com>
2018-01-05 17:49:50 +0200

    some changes

--- src/app/pages/reports-page/mvc/model.js
+++ src/app/pages/reports-page/mvc/model.js
@@ -2,19 +2,71 @@ export class Model{
   constructor(data){
     this.data = data.filter((item) => {
       return item.isActive === false
-    })
+    });
+  }
+
+  getCurrentWeek(){
+    let todayDate = new Date();
+    const weekDay = todayDate.getDay();
+
+    todayDate.setDate(todayDate.getDate() - weekDay);
+
+    return todayDate;
+  }
+
+  getWeekData(){
+    const currentWeekStart = this.getCurrentWeek();
+    const currentWeekEnd = this.getCurrentWeek();
+    currentWeekEnd.setDate(currentWeekStart.getDate() + 7);
+
+    let numberOfTasks = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]];
+
+    let data = this.data
+      .filter((item) => {
+        const taskDate = new Date(item.startDate);
+
+        return taskDate >= currentWeekStart && taskDate <= currentWeekEnd;
+      })
+      .reduce((prev, item) => {
+        const taskDay = new Date(item.startDate).getDate();
+
+        prev[item.priority - 1][taskDay]++;
+
+        return prev;
+      }, numberOfTasks);
+
+    return data;
+  }
+
+  getTodayData(){
+    const todayDate = new Date().getDate();
+    const currentMonth = new Date().getMonth();
+
+    let data = this.data
+      .filter((item) => {
+        const taskDate = new Date(item.startDate).getDate();
+          const taskMonth = new Date(item.startDate).getMonth();
+
+        return taskDate === todayDate && taskMonth === currentMonth;
+      })
+
       .map((item) => {
         return +item.priority
-    })
+      })
+
       .sort()
+
       .reduce((prev, current) => {
         prev[current - 1]++;
         return prev;
       }, [0, 0, 0, 0, 0]);
-    console.log(this.data)
+
+    return data;
   }
 
   sendData(){
-    return this.data;
+    const data = this.getWeekData();
+
+    return data;
   }
 }
--- src/app/pages/reports-page/mvc/view.js
+++ src/app/pages/reports-page/mvc/view.js
@@ -7,17 +7,120 @@ export class View {
     this.controller = new Controller(data);
   }
 
-  drawChart(data){
+  drawWeeklyChart(data) {
+    Highcharts.chart('report', {
+      title: '',
+      chart: {
+        type: 'column',
+        backgroundColor: '#2A3F50',
+        width: 570,
+        height: 350
+      },
+      xAxis: {
+        type: 'category',
+        categories: [ 'SUN', 'MON', 'THU', 'WED', 'THR', 'FRI', 'SUT',],
+        lineColor: '#fff',
+        tickWidth: 0,
+        labels: {
+          style: {
+            color: '#fff',
+            fontFamily: 'PT Sans, sans-serif',
+            fontSize: '13px'
+          }
+        },
+      },
+      yAxis: {
+        title: '',
+        gridLineColor: '#345168',
+        lineColor: '#fff',
+        lineWidth: 1,
+        labels: {
+          style: {
+            color: '#fff',
+            fontFamily: 'PT Sans, sans-serif',
+            fontSize: '13px'
+          }
+        }
+      },
+      legend: {
+        symbolRadius: 0,
+        symbolHeight: 8,
+        symbolWidth: 8,
+        margin: 50,
+        style: {
+          display: 'flex',
+          alignItems: 'center'
+        },
+        itemStyle: {
+          color: '#8da5b8',
+          fontWeight: 'normal',
+          fontSize: 12
+        },
+        itemHoverStyle: {
+          color: '#82c7e0'
+        }
+      },
+      plotOptions: {
+        series: {
+          borderWidth: 0,
+          pointWidth: 30,
+          groupPadding: 0.5,
+          dataLabels: {
+            enabled: false,
+            format: '{point.y}'
+          }
+        },
+        column: {
+          stacking: 'normal'
+        }
+      },
 
+      tooltip: {
+        backgroundColor: '#dbeaf5',
+        borderColor: '#dbeaf5',
+        formatter: function () {
+          return `<span class="report__tooltip-title">${this.series.name}</span><br><span class="report__tooltip-span">Tasks</span>: ${this.series.data[0].y}<br/>`
+        }
+      },
+
+      series: [
+        {
+          name: 'URGENT',
+          data: data[0],
+          stack: 0,
+          color: '#F75C4C'
+        },
+        {
+          name: 'HIGH',
+          data: data[1],
+          stack: 0,
+          color: '#FFA841'
+        },
+        {
+          name: 'MIDDLE',
+          data: data[2],
+          stack: 0,
+          color: '#FDDC43'
+        },
+        {
+          name: 'LOW',
+          data: data[3],
+          stack: 0,
+          color: '#1ABC9C'
+        },
+        {
+          name: 'FAILED',
+          data: data[4],
+          stack: 0,
+          color: '#8da5b8'
+        }
+      ]
+    });
   }
 
-  render() {
-    const root = document.getElementById('root');
 
-    root.innerHTML = Template();
 
-    const data = this.controller.receiveData();
-
+  drawDailyChart(data) {
     Highcharts.chart('report', {
       title: '',
       chart: {
@@ -94,24 +197,9 @@ export class View {
         name: 'URGENT',
         data: [{
           name: 'URGENT',
-          y: 1,
-          x: 0
-        },
-          {
-            name: 'URGENT',
-            y: 4,
-            x: 0
-          },
-          {
-            name: 'URGENT',
-            y: 6,
-            x: 0
-          }],
-        color: '#F75C4C',
-        zones: [{
-          value: 4,
-          color: 'blue'
-        }]
+          y: data[0]
+        }],
+        color: '#F75C4C'
       }, {
         name: 'HIGH',
         data: [{
@@ -142,5 +230,17 @@ export class View {
         color: '#8da5b8'
       }]
     });
+  }
+
+  render() {
+    const root = document.getElementById('root');
+
+    root.innerHTML = Template();
+
+    const data = this.controller.receiveData();
+
+    console.log(data);
+
+    this.drawWeeklyChart(data);
   }
 }
