2a76c6e5c70dbfd25b2bd2549641e9d4c7ff2f70
Serhii Nosko1 <serhii_nosko1@epam.com>
2018-01-12 17:52:58 +0200

    starting kit

--- karma.conf.js
+++ karma.conf.js
@@ -1,79 +1,86 @@
-const path = require('path');
+/*const path = require('path');
+
+ module.exports = function (config) {
+ config.set({
+ frameworks: ['jasmine'],
+ browsers: ['Chrome'],
+ files: [
+ 'tests/app/components/single-task/modelSpec.js'
+ ],
+ // coverage reporter generates the coverage
+ reporters: ['progress'],
+
+ preprocessors: {
+ 'tests/app/components/single-task/modelSpec.js': 'webpack'
+ },
+ webpack: {
+ module: {
+ rules: [
+ // skip assest
+ {test: /\.less$/, loader: 'ignore-loader'},
+ {test: /\.css$/, loader: 'ignore-loader'},
+ {test: /\.hbs$/, loader: 'ignore-loader'},
+ // instrument only testing sources with Istanbul
+ {
+ test: /\.js$/,
+ use: {
+ loader: 'istanbul-instrumenter-loader',
+ options: {esModules: true}
+ },
+ enforce: 'post',
+ exclude: /node_modules|test|\.spec\.js$|^app.js$/
+ }]
+ },
+ devtool: 'inline-source-map'
+ },
+ // optionally, configure the reporter
+ coverageIstanbulReporter: {
+ reports: ['html', 'lcovonly', 'text-summary'],
+ dir: path.join(__dirname, 'coverage'),
+ fixWebpackSourcePaths: true,
+ html: {
+ // outputs the report in ./coverage/html
+ subdir: 'html'
+ }
+ }
+ });
+ };*/
 
 module.exports = function (config) {
   config.set({
     frameworks: ['jasmine'],
-    browsers: ['Chrome'],
     files: [
+      "src/app/components/single-task/mvc/model.js",
       'tests/app/components/single-task/modelSpec.js'
     ],
-    // coverage reporter generates the coverage
-    reporters: ['progress'],
 
-    /*preprocessors: {
-      'test/index.js': 'webpack'
-    },*/
-    webpack: {
-      module: {
-        rules: [
-          // skip assest
-          {test: /\.less$/, loader: 'ignore-loader'},
-          {test: /\.css$/, loader: 'ignore-loader'},
-          {test: /\.hbs$/, loader: 'ignore-loader'},
-          // instrument only testing sources with Istanbul
-          {
-            test: /\.js$/,
-            use: {
-              loader: 'istanbul-instrumenter-loader',
-              options: {esModules: true}
-            },
-            enforce: 'post',
-            exclude: /node_modules|test|\.spec\.js$|^app.js$/
-          }]
-      },
-      devtool: 'inline-source-map'
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-jasmine',
+      'karma-coverage',
+      'karma-babel-preprocessor'
+    ],
+
+    // coverage reporter generates the coverage
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'src/app/components/single-task/mvc/model.js': ['babel']
     },
-    // optionally, configure the reporter
-    coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly', 'text-summary'],
-      dir: path.join(__dirname, 'coverage'),
-      fixWebpackSourcePaths: true,
-      html: {
-        // outputs the report in ./coverage/html
-        subdir: 'html'
+
+    babelPreprocessor: {
+      options: {
+        presets: ['env']
       }
+    },
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
     }
   });
 };
-
-/*module.exports = function (config) {
- config.set({
- frameworks: ['jasmine'],
- files: [
- "src/!**!/!*.js",
- "tests/app/!**!/!*Spec.js"
- ],
-
- plugins: [
- 'karma-chrome-launcher',
- 'karma-jasmine',
- 'karma-coverage'
- ],
-
- // coverage reporter generates the coverage
- reporters: ['progress', 'coverage'],
-
- preprocessors: {
- // source files, that you wanna generate coverage for
- // do not include tests or libraries
- // (these files will be instrumented by Istanbul)
- 'src/!**!/!*.js': ['coverage']
- },
-
- // optionally, configure the reporter
- coverageReporter: {
- type: 'html',
- dir: 'coverage/'
- }
- });
- };*/
